ea92b3ce47608c48c8052050695862a69fd29630
adolwick <dolwick.a@gmail.com>
2019-12-12 19:46:31 -0500

    various edits

--- dolwick_project/splitdate.py
+++ dolwick_project/splitdate.py
@@ -1,12 +1,14 @@
+import pandas as pd
 def splitdate(d, y):
+    '''
+    The splitdate function allows me to split a column of dates within a pandas dataframe into three columns -- year, month, and day -- and to immediately concatenate those columns with the original dataframe and name them accordingly.
+
+    d = Pandas dataframe
+    y = column of dates in yyyy-mm-dd format within d
+    '''
+
     ds = y.str.split(pat = "-", expand = True)
     dsd = pd.concat([d, ds], axis=1, sort=False)
     ymd = dsd.rename(columns={0: "year", 1: "month", 2: "day"})
     return ymd
 
-'''
-The splitdate function allows me to split a column of dates within a pandas dataframe into three columns -- year, month, and day -- and to immediately concatenate those columns with the original dataframe and name them accordingly. Pandas must be installed to run this function.
-
-d = Pandas dataframe
-y = column of dates in yyyy-mm-dd format within d
-'''
