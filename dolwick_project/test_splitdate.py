1aeedc25df274953e3d77880034c09e685c80773
adolwick <dolwick.a@gmail.com>
2019-12-12 20:27:38 -0500

    tests the splitdate function

--- dolwick_project/test_splitdate.py
+++ dolwick_project/test_splitdate.py
@@ -3,9 +3,12 @@ import pandas as pd
 from . import splitdate
 
 def tsd():
-    d = {'Date': [2019-12-12]}
+    '''
+    This function tests splitdate.splitdate. There are no arguments.
+    '''
+    d = {'Date': ['2019-12-12']}
     begin = pd.DataFrame(data=d)
-    e = {'Date': [2019-12-12], 'year': [2019], 'month': [12], 'day': [12]}
+    e = {'Date': ['2019-12-12'], 'year': ['2019'], 'month': ['12'], 'day': ['12']}
     expected = pd.DataFrame(data=e)
-    out = splitdate.splitdate(begin, date)
-    assert expected == out
+    out = splitdate.splitdate(begin, begin['Date'])
+    pd.testing.assert_frame_equal(out, expected)
